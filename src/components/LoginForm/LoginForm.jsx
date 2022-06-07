8dbe7e89faecb30ab0ca34487506a74077f67e90
Sior Meir <siorm2310@gmail.com>
2022-06-07 22:36:11 +0300

    added functionality to LoginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import {React} from 'react'
+
 
 function LoginForm() {
+  
+  const tryToAuthenticate = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    const response = await fetch("http://localhost:3000/login",{
+      method : "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username : data.get('username'),
+        password : data.get('password')
+      }),
+    })
+
+    const token = await response.json()
+  }  
+
   return (
     <div className="login-form">
-    <form>
+    <form onSubmit={tryToAuthenticate}>
       <h1>Welcome to News Analyzer</h1>
+      <h2>Let's see if we know each other</h2>
       <div className="content">
         <div className="input-field">
-          <input type="email" placeholder="Email" autoComplete="nope"/>
+          <input type="text" placeholder="Username" id='username' name='username' autoComplete="nope" required/>
         </div>
         <div className="input-field">
-          <input type="password" placeholder="Password" autoComplete="new-password"/>
+          <input type="password" placeholder="Password" id='password' name='password' autoComplete="new-password" required/>
         </div>
       </div>
       <div className="action">
